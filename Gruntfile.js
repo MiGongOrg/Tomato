58468cf66a37268c99307eccac615e99d1fff96e
Ming <muvoid@foxmail.com>
2016-10-12 10:53:05 +0800

    *修改 gruntfile.js 缩进格式

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,23 +1,23 @@
 module.exports = function (grunt) {
-	grunt.initConfig({
-		pkg: grunt.file.readJSON('package.json'),
-		browserSync: {
-			bsFiles: {
-				src: ['./*.html','src/css/*.less']
-			},
-			options: {
-				server: {
-					baseDir: './'
-				}
-			}
-		}
-	});
-	// 加载插件 Browser-sync
-	grunt.loadNpmTasks('grunt-browser-sync');
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    browserSync: {
+      bsFiles: {
+        src: ['./*.html','src/css/*.less']
+      },
+      options: {
+        server: {
+          baseDir: './'
+        }
+      }
+    }
+  });
+  // 加载插件 Browser-sync
+  grunt.loadNpmTasks('grunt-browser-sync');
 
-	// 定义单个执行任务
-	grunt.registerTask('x-sync', ['browserSync']);
+  // 定义单个执行任务
+  grunt.registerTask('x-sync', ['browserSync']);
 
-	// 定义默认任务
-	grunt.registerTask('default', ['x-sync']);
+  // 定义默认任务
+  grunt.registerTask('default', ['x-sync']);
 }
